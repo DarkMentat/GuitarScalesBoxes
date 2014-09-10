8778d1b48e9c66ba8dfb5235363a193639b40bd9
Mentat <indraua@gmail.com>
2014-09-11 00:19:10 +0300

    Initial commit

---
+++ src/org/darkmentat/GuitarScalesBoxes/Activities/Main.java
@@ -0,0 +1,18 @@
+package org.darkmentat.GuitarScalesBoxes.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.darkmentat.GuitarScalesBoxes.R;
+
+public class Main extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
