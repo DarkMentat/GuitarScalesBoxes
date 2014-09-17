f7da10b2bc6449c566a09be9b7d5aa3903875e5d
Mentat <indraua@gmail.com>
2014-09-17 19:39:00 +0300

    notes in the guitar

--- src/org/darkmentat/GuitarScalesBoxes/Activities/Main.java
+++ src/org/darkmentat/GuitarScalesBoxes/Activities/Main.java
@@ -5,21 +5,17 @@ import android.os.Bundle;
 import org.darkmentat.GuitarScalesBoxes.Controls.GuitarView.FretBoard;
 import org.darkmentat.GuitarScalesBoxes.Controls.GuitarView.GuitarView;
 import org.darkmentat.GuitarScalesBoxes.Model.GuitarModel;
+import org.darkmentat.GuitarScalesBoxes.Model.GuitarSetting;
 import org.darkmentat.GuitarScalesBoxes.R;
 
 public class Main extends Activity
 {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    @Override public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
         GuitarView g =(GuitarView) findViewById(R.id.main_gvGuitar);
-        FretBoard fretBoard = new GuitarModel(6, 24);
+        FretBoard fretBoard = new GuitarModel(GuitarSetting.Default, 24);
         g.setFretBoard(fretBoard);
     }
 }
--- src/org/darkmentat/GuitarScalesBoxes/Model/GuitarModel.java
+++ src/org/darkmentat/GuitarScalesBoxes/Model/GuitarModel.java
@@ -4,7 +4,15 @@ import org.darkmentat.GuitarScalesBoxes.Controls.GuitarView.FretBoard;
 
 public class GuitarModel extends FretBoard
 {
-    public GuitarModel(int stringCount, int fretCount) {
-        super(stringCount, fretCount);
+    public GuitarModel(GuitarSetting setting, int fretCount) {
+        super(setting.StringCount, fretCount);
+
+        Tab = new NoteModel[fretCount][setting.StringCount];
+
+        System.arraycopy(setting.StartNotes, 0, Tab[0], 0, setting.StartNotes.length);
+
+        for (int x = 1; x < fretCount; x++)
+            for(int y = 0; y < setting.StartNotes.length; y++)
+                Tab[x][y] = ((NoteModel)Tab[x-1][y]).getNext();
     }
 }
