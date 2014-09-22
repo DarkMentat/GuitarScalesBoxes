14205e6ffb60c9e08c99a279761925ff8691f034
Mentat <indraua@gmail.com>
2014-09-22 23:19:23 +0300

    Draft menu for selecting scales

--- src/org/darkmentat/GuitarScalesBoxes/Activities/Main.java
+++ src/org/darkmentat/GuitarScalesBoxes/Activities/Main.java
@@ -3,6 +3,8 @@ package org.darkmentat.GuitarScalesBoxes.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import org.darkmentat.GuitarScalesBoxes.Controls.GuitarView.GuitarView;
 import org.darkmentat.GuitarScalesBoxes.Model.GuitarModel;
 import org.darkmentat.GuitarScalesBoxes.Model.GuitarSetting;
@@ -18,7 +20,16 @@ public class Main extends Activity
 
         GuitarView g =(GuitarView) findViewById(R.id.main_gvGuitar);
         g.setFretBoard(GuitarModel);
+    }
 
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Select scale");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
         startActivity(new Intent(this, ScalesOverview.class));
+
+        return super.onOptionsItemSelected(item);
     }
 }
