afe6118211ccde627e32fd79329a57ec160d1cc6
Mentat <indraua@gmail.com>
2014-11-06 00:23:00 +0200

    Scales activity customized for tablets

--- src/org/darkmentat/GuitarScalesBoxes/Activities/Scales.java
+++ src/org/darkmentat/GuitarScalesBoxes/Activities/Scales.java
@@ -1,11 +1,18 @@
 package org.darkmentat.GuitarScalesBoxes.Activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import org.darkmentat.GuitarScalesBoxes.Fragments.ScaleDescription;
 import org.darkmentat.GuitarScalesBoxes.Fragments.ScalesOverview;
 import org.darkmentat.GuitarScalesBoxes.Fragments.SelectNote;
 import org.darkmentat.GuitarScalesBoxes.R;
 import org.holoeverywhere.app.Activity;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+import static android.widget.LinearLayout.LayoutParams;
 
 public class Scales extends Activity implements ScalesOverview.ScalesOverviewListener
 {
@@ -23,13 +30,43 @@ public class Scales extends Activity implements ScalesOverview.ScalesOverviewLis
         bundle.putInt("ScaleIndex", scaleIndex);
         SelectNote frag = new SelectNote();
         frag.setArguments(bundle);
-        getSupportFragmentManager().beginTransaction().replace(R.id.leftFragment, frag).addToBackStack(null).commit();
+
+        int sideFragment = isScreenLarge()? R.id.rightFragment : R.id.leftFragment;
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction().replace(sideFragment, frag);
+        if(isScreenLarge() && getSupportFragmentManager().findFragmentById(R.id.rightFragment) == null)
+            transaction.addToBackStack(null);
+        transaction.commit();
+
+        findViewById(R.id.rightFragment).setLayoutParams(new LayoutParams(MATCH_PARENT, WRAP_CONTENT, 1));
     }
     @Override public void onScaleAskedForDescription(int scaleIndex) {
         Bundle bundle = new Bundle();
         bundle.putInt("ScaleIndex", scaleIndex);
         ScaleDescription frag = new ScaleDescription();
         frag.setArguments(bundle);
-        getSupportFragmentManager().beginTransaction().replace(R.id.leftFragment, frag).addToBackStack(null).commit();
+
+        int sideFragment = isScreenLarge()? R.id.rightFragment : R.id.leftFragment;
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction().replace(sideFragment, frag);
+        if(isScreenLarge() && getSupportFragmentManager().findFragmentById(R.id.rightFragment) == null)
+            transaction.addToBackStack(null);
+        transaction.commit();
+
+        findViewById(R.id.rightFragment).setLayoutParams(new LayoutParams(MATCH_PARENT, WRAP_CONTENT, 1));
+    }
+
+    @Override public void onBackPressed() {
+        super.onBackPressed();
+        Fragment frag = getSupportFragmentManager().findFragmentById(R.id.rightFragment);
+        if(frag != null)
+            getSupportFragmentManager().beginTransaction().remove(frag).commit();
+        findViewById(R.id.rightFragment).setLayoutParams(new LayoutParams(0, WRAP_CONTENT, 1));
+    }
+
+    private boolean isScreenLarge() {
+        return (getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
     }
 }
